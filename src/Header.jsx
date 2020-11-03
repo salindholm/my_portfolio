a64877c08ba77420c6a2ab26602e65dd4186bc1f
Sanne <sannelindholm@hotmail.com>
2020-11-03 14:43:55 +0100

    Adding Link and NavLink components to Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Menu, Segment } from "semantic-ui-react"
+import { Menu, Segment } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Segment inverted>
       <Menu inverted secondary>
         <Menu.Item
-        name="My portfolio"
+          id="header"
+          name="my portfolio"
+          as={Link}
+          to={{ pathname: "/" }}
+        />
+        <Menu.Item
+          id="about-tab"
+          name="my portfolio"
+          as={NavLink}
+          to={{ pathname: "/about" }}
+        />
+        <Menu.Item
+          id="projects-tab"
+          name="my portfolio"
+          as={NavLink}
+          to={{ pathname: "/projects" }}
         />
       </Menu>
     </Segment>
   );
 };
 
-export default Header
+export default Header;
