8ab27b883aab5f5b2b0f22fa1d43308c5a7abeb3
Sanne <sannelindholm@hotmail.com>
2020-11-03 15:25:27 +0100

    Refactoring Projects component in Projects.jsx from stateless component to use class.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>
-        Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident copporis fuga saepe distinctio ipsam? Et quos haram excepturi dolorum moestias?
-      </p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "Fizzbuzz",
+      },
+    ],
+  };
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
 
 export default Projects;
