2d39610686f19638b4910d646ef2bc5f953d320e
Sanne <sannelindholm@hotmail.com>
2020-11-08 15:52:07 +0100

    Updating description for userCanSeeCv test and test passing

--- cypress/integration/userCanSeeCv.feature.js
+++ cypress/integration/userCanSeeCv.feature.js
@@ -8,7 +8,7 @@ describe("User can see list of Cv", () => {
     cy.get("#cv-1").within(() => {
       cy.get(".image").should("exist");
       cy.get(".header").should("contain", "Experience");
-      cy.get(".description").should("contain", "A digital address book!"
+      cy.get(".description").should("contain", "Project Manager at Egencia Sweden AB coordinating group travels for companies."
       );
     });
   });
@@ -17,7 +17,7 @@ describe("User can see list of Cv", () => {
     cy.get("#cv-2").within(() => {
       cy.get(".image").should("exist");
       cy.get(".header").should("contain", "Education");
-      cy.get(".description").should("contain", "A digital address book!"
+      cy.get(".description").should("contain", "International Travel Consultant with IATA and UFTAA qualifications."
       );
     });
   });
