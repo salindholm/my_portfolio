8939d5163a1061731f9285ba2876cce64fcec49d
Sanne <sannelindholm@hotmail.com>
2020-11-09 08:44:32 +0100

    Updating Home information and styling

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -4,7 +4,14 @@ import { Container } from "semantic-ui-react";
 const Hello = () => {
   return (
     <Container >
-      <h1 id="hello-header">Welcome to my portfolio!</h1>
+      <h1 id="hello-header">Hello!</h1>
+      <p id="hello-text">
+        My name is Sanne Lindholm, a web developer based in Stockholm. 
+        <br />
+        This is a portfolio to showcase my work and some info about me.
+        <br />
+        Enjoy!
+      </p>
     </Container>
   );
 };
