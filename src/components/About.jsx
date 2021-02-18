12b5e993ca46208aa9e5c9c43e3246727548b7d2
Sanne <sannelindholm@hotmail.com>
2021-02-18 17:30:10 +0100

    Updating about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ const About = () => {
           floated='left'
         />
         <Container className='about-content'>
-        <p>I'm a 26 y/o girl from the Northern parts of Sweden who is passionate about traveling and exploring the world. I’ve up to this point been in 38 countries and after High School I went to live in Australia and England for a year each. Back in Sweden I started to work in the travel industry, but due to Covid-19 I lost my job.</p>
-      <p>So my focus switched to programming. I’m a really creative person who enjoys problem solving, therefor I think it's a really good match. I’ve just finished a 12 week Full Stack Web Developer Boot Camp and I'm very exited to start my journey in this new industry.</p>
+        <p>I'm a 26 y/o girl from the Northern parts of Sweden who enjoys traveling and exploring the world when I have some time off. I’ve up to this point been in 38 countries and after High School I went to live in Australia and England for a year each. Back in Sweden I started to work in the travel industry, but due to Covid-19 I lost my job.</p>
+      <p>So my focus switched to programming, I’m a really creative person who enjoys problem solving, therefore I think it's a really good match. I’ve just finished a 12-week Full Stack Web Developer Boot Camp and I'm very excited to start my journey in this new innovating industry.</p>
         </Container>
       </Container>
     </>
